7d53519c7080e85782f7b2cff6b17d120c6b558e
ipsluizeduardo <ipsluizeduardo@gmail.com>
2023-05-12 17:11:43 -0400

    desafio

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http'
 
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { ComponentsComponent } from './components/components.component';
+import { ServicesComponent } from './services/services.component';
+import { CardComponent } from './components/card/card.component';
+import { from } from 'rxjs';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ComponentsComponent,
+    ServicesComponent,
+    CardComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
